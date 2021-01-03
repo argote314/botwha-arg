04295a30be663d2556bd77af7d6179c795ba2c8b
argote314 <76407732+argote314@users.noreply.github.com>
2021-01-03 16:12:10 -0500

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,25 @@
+const help = (prefix) => {
+	 return `
+┏ ❣ 🌺⃟⃘͜͡꙰❦ོ❮ΝᎪᏦᎪΝϴ-ᎷᏆᏦႮ❯░⃟🌺 ❣
+╿
+┷┯ ☾ Group Commands ☽
+   ╽
+   ┠❥ *.add 62858xxxxx*
+   ┠❥ *.kick @tagmember*
+   ┠❥ *.promote @tagmember*
+   ┠❥ *.demote @tagadmin*
+   ┠❥ *.linkgroup*
+   ╿
+┯┷ ☾ otros coamandos ☽
+╽
+┠❥ *.sticker [imagen]*
+┠❥ *.sticker [gif]*
+┠❥ *.loli*
+┠❥ *.info*
+┠❥ *.meme*
+┠❥ *.gtts [idioma][texto]*
+╿
+┷┯ 
+╰╼❥ PROXIMAMENTE MAS!!.`
+}
+exports.help = help
